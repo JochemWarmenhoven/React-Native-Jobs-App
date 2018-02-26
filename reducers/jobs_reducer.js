6a204fb5adb4e162b9fe855fb1a55797896f0068
jochem Warmenhoven <jochemwarmenhoven@jochems-MacBook-Pro.local>
2018-02-26 10:33:47 +0100

    so far so good

--- reducers/jobs_reducer.js
+++ reducers/jobs_reducer.js
@@ -5,11 +5,14 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+
 	switch (action.type) {
 		case FETCH_JOBS:
 			return action.payload;
 
 		default:
+			console.log('current state', state);
+
 			return state;
 	}
 };
--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
+
+import Swipe from '../components/Swipe';
+
 
 class DeckScreen extends Component {
+	renderCard(job) {
+		return (
+			<Card title={job.jobtitle}>
+				<View style={styles.detailWrapper}>
+					<Text>{job.company}</Text>
+					<Text>{job.formattedRelativeTime}</Text>
+				</View>
+			</Card>
+		);
+	}
 	render() {
+		console.log('current jobs', this.props.jobs);
+
 		return (
 			<View>
-				<Text> DeckScreen </Text>
-				<Text> DeckScreen </Text>
-				<Text> DeckScreen </Text>
-				<Text> DeckScreen </Text>
-				<Text> DeckScreen </Text>
-				<Text> DeckScreen </Text>
+				<Swipe
+					data={this.props.jobs}
+					renderCard={this.renderCard}
+				/>
 			</View>
 		);
 	}
 }
 
-export default DeckScreen;
+const styles = {
+	detailWrapper: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginBottom: 10,
+	},
+};
+
+function mapStateToProps({ jobs }) {
+	return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps)(DeckScreen);
